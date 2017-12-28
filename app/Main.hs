f790f8dce7640d2a9a82604181c1d1e8f3ea3da1
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-12-28 15:20:34 +0100

    it compiles, but it segfaults

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,105 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs            #-}
-{-# LANGUAGE RankNTypes       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
 module Main where
 
 import           CLI
+import           Control.Lens
 import           Control.Monad.Reader
 import           Data.Default                (def)
+import           Data.Maybe                  (fromJust)
 import           GHC.Conc
 import           Lib
 import           Mockable                    (Production, runProduction)
 import           Options.Generic
-import           Pos.Launcher                (withConfigurations)
+import           Pos.Client.CLI              (CommonArgs (..), CommonNodeArgs (..),
+                                              NodeArgs (..), getNodeParams, gtSscParams)
+import           Pos.DB.Rocks.Functions
+import           Pos.Launcher
 import           Pos.Launcher.Configuration
+import           Pos.Network.CLI
+import           Pos.Network.Types
+import           Pos.Ssc.Extra
+import           Pos.Ssc.SscAlgo
+import           Pos.Util.JsonLog
+import           Pos.Util.UserSecret         (usVss)
+import           Pos.Wallet.SscType          (WalletSscType)
 import           Pos.Wallet.Web.Mode
 import           Pos.Wallet.Web.State.Acidic
 import           Pos.Wallet.Web.State.State  (WalletState)
+import           Pos.WorkMode
+import           System.IO
+import           System.Wlog.CanLog
 import           System.Wlog.LoggerName
 import           System.Wlog.LoggerNameBox
 
+newRealModeContext :: HasConfigurations => ConfigurationOptions -> Production (RealModeContext WalletSscType)
+newRealModeContext confOpts = do
+    let nodeArgs = NodeArgs {
+      sscAlgo            = GodTossingAlgo
+    , behaviorConfigPath = Nothing
+    }
+    let networkOps = NetworkConfigOpts {
+          networkConfigOptsTopology = Nothing
+        , networkConfigOptsKademlia = Nothing
+        , networkConfigOptsSelf     = Nothing
+        , networkConfigOptsPort     = 3030
+        , networkConfigOptsPolicies = Nothing
+        }
+    let cArgs@CommonNodeArgs {..} = CommonNodeArgs {
+           dbPath                 = "node-db"
+         , rebuildDB              = True
+         , devSpendingGenesisI    = Nothing
+         , devVssGenesisI         = Nothing
+         , keyfilePath            = "keys"
+         , backupPhrase           = Nothing
+         , externalAddress        = Nothing
+         , bindAddress            = Nothing
+         , peers                  = mempty
+         , networkConfigOpts      = networkOps
+         , jlPath                 = Nothing
+         , kademliaDumpPath       = "kademlia"
+         , commonArgs             = CommonArgs {
+               logConfig            = Nothing
+             , logPrefix            = Nothing
+             , reportServers        = mempty
+             , updateServers        = mempty
+             , configurationOptions = confOpts
+             }
+         , updateLatestPath       = "update"
+         , updateWithPackage      = True
+         , noNTP                  = True
+         , route53Params          = Nothing
+         , enableMetrics          = False
+         , ekgParams              = Nothing
+         , statsdParams           = Nothing
+         , cnaDumpGenesisDataPath = Nothing
+         }
+    nodeParams <- getNodeParams cArgs nodeArgs
+    let vssSK = fromJust $ npUserSecret nodeParams ^. usVss
+    let gtParams = gtSscParams cArgs vssSK (npBehaviorConfig nodeParams)
+    bracketNodeResources @WalletSscType @IO nodeParams gtParams $ \NodeResources{..} ->
+        RealModeContext <$> pure nrDBs
+                        <*> pure nrSscState
+                        <*> pure nrTxpState
+                        <*> pure nrDlgState
+                        <*> jsonLogConfigFromHandle stdout
+                        <*> pure (LoggerName "dbgen")
+                        <*> pure nrContext
+                        <*> initQueue (defaultNetworkConfig (TopologyAuxx mempty)) Nothing
 
-walletRunner :: HasConfigurations => WalletWebModeContext -> UberMonad a -> IO a
-walletRunner wwmc act = runProduction $
+
+walletRunner :: HasConfigurations => ConfigurationOptions -> UberMonad a -> IO a
+walletRunner confOpts act = runProduction $ do
+    wwmc <- WalletWebModeContext <$> newWalletState
+                                 <*> liftIO (newTVarIO def)
+                                 <*> newRealModeContext confOpts
     runReaderT act wwmc
 
-newWalletState :: HasConfigurations => IO WalletState
-newWalletState = openState True "./wallet-db"
+newWalletState :: (MonadIO m, HasConfigurations) => m WalletState
+newWalletState = liftIO $ openState True "./wallet-db"
 
 defLoggerName :: LoggerName
 defLoggerName = LoggerName "dbgen"
@@ -39,10 +114,10 @@ newConfig = defaultConfigurationOptions {
   }
 
 main :: IO ()
-main = withConfigurations newConfig $ do
-  cli@CLI{..} <- getRecord "DBGen"
-  spec <- loadGenSpec config
-  ctx <- WalletWebModeContext <$> newWalletState
-                              <*> newTVarIO def
-                              <*> return (error "testRealModeContext is currently unimplemented")
-  walletRunner ctx (generate spec)
+main = do
+  let cfg = newConfig
+  withConfigurations cfg $ do
+    cli@CLI{..} <- getRecord "DBGen"
+    spec <- loadGenSpec config
+    walletRunner cfg (generate spec)
+
